4aef8237c140950f7403d723ada449165a85d70c
daniCodes1 <danirenn16@gmail.com>
2026-02-22 08:51:06 -0800

    Change feature

--- backend/features.py
+++ backend/features.py
@@ -8,12 +8,12 @@ Feature engineering utilities
 TARGET = "default.payment.next.month"
 
 # Continuous features
-DISCRETIZATION_FEATS = ["AGE", "LIMIT_BAL"]
+DISCRETIZATION_FEATS = ["AGE"]
 
 # Monetary amounts, scaled
 NUMERIC_FEATS = [
     "BILL_AMT1","BILL_AMT2","BILL_AMT3","BILL_AMT4","BILL_AMT5","BILL_AMT6",
-    "PAY_AMT1","PAY_AMT2","PAY_AMT3","PAY_AMT4","PAY_AMT5","PAY_AMT6",
+    "PAY_AMT1","PAY_AMT2","PAY_AMT3","PAY_AMT4","PAY_AMT5","PAY_AMT6", "LIMIT_BAL"
 ]
 
 ORDINAL_FEATS = ["PAY_0","PAY_2","PAY_3","PAY_4","PAY_5","PAY_6"]
